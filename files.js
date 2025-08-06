0d8db8d368f9ee70a6d92386791e820918341a1e
hikaru107 <153613615+hikaru107@users.noreply.github.com>
2025-08-06 18:42:38 +0000

    login.js

---
+++ files.js
@@ -0,0 +1,47 @@
+import Login from './login';
+import Cadastro from './cadastro';
+import Escolha from './escolha';
+import Tutorial1 from './tutorial1';
+import Tutorial2 from './tutorial2';
+import Tutorial3 from './tutorial3';
+import Tutorial4 from './tutorial4';
+import Diario from './diario';
+import DiarioNovo from './diarioNovo';
+import ConsultasFuturas from './consultasFuturas';
+import ConsultasPassadas from './consultasPassadas';
+import Midia from './midia';
+import Desafios from './desafios';
+import DesafiosNovo from './desafiosNovo';
+import Usuario from './usuario';
+import UsuarioDados from './usuarioDados';
+import InfMedico from './infMedico';
+import Agendamento from './agendamento';
+import Notificacao from './notificacao';
+
+export {
+  Login,
+  Cadastro,
+  Escolha,
+
+  Tutorial1,
+  Tutorial2,
+  Tutorial3,
+  Tutorial4,
+
+  Diario,
+  DiarioNovo,
+
+  ConsultasFuturas,
+  InfMedico,
+  Agendamento,
+  ConsultasPassadas,
+
+  Midia,
+
+  Desafios,
+  DesafiosNovo,
+
+  Usuario,
+  UsuarioDados,
+  Notificacao,
+}
