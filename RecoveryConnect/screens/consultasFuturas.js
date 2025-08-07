87f9e68c1edf0ac51251bf1fd81a8f2d17309a06
hikaru107 <153613615+hikaru107@users.noreply.github.com>
2025-08-07 11:44:51 +0000

    Consultas

--- RecoveryConnect/screens/consultasFuturas.js
+++ RecoveryConnect/screens/consultasFuturas.js
@@ -1,9 +1,270 @@
-import { View } from 'react-native';
+import {View, Text, TouchableOpacity, Image, StyleSheet, FlatList} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {useState} from 'react';
 
-export default function ConsultasFuturas(){
-    return (
-        <View>
+export default function ConsultasFuturas({navigation}) {
+  const data = Array.from({ length: 31 }, (_, i) => `${i + 1}`);
 
+  const renderItemDay = ({ item }) => (
+    <TouchableOpacity style={style.item} onPress={() => setDiaSelecionado(item)}>
+      <Text style={style.itemText}>{item}</Text>
+    </TouchableOpacity>
+  );
+
+  const [diaSelecionado, setDiaSelecionado] = useState(1);
+
+  const medPresentes = {
+   1: [
+      {
+        id: 1,
+        foto: require('../imagens/medico/mdc1.png'),
+        rating: '⭐⭐⭐⭐⭐',
+        quantR: '(102)',
+        nome: 'Marcos Henrique Santos',
+        hosp: 'Psicologia Taboão da Serra',
+        endereco: 'Av. Vida Nova, 28 - 901 A, Jardim Maria Rosa, Taboão da Serra - SP, 06768-000',
+        valor: '180,00',
+        formacao: 'Faculdade de Medicina da Universidade de São Paulo',
+        dias: 'Domingo a Sexta',
+        hora: '9:00 - 18:00',
+        formato: 'Online / Presencial',
+      },
+      {
+        id: 2,
+        foto: require('../imagens/medico/mdc2.png'),
+        rating: '⭐⭐⭐⭐⭐',
+        quantR: '(284)',
+        nome: 'Laurêncio Gonzales Pinto',
+        hosp: 'Hospital Elma Mário',
+        endereco: 'Av. Armando de Andrade, 346, Parque Santos Dumont, Taboão da Serra - SP, 06754-210',
+        valor: '106,90',
+        formacao: 'Faculdade de Medicina da Universidade de São Paulo',
+        dias: 'Segunda a Sexta',
+        hora: '9:00 - 18:00',
+        formato: 'Online / Presencial',
+      },
+      {
+        id: 3,
+        foto: require('../imagens/medico/mdc3.png'),
+        rating: '⭐⭐⭐⭐⭐',
+        quantR: '(157)',
+        nome: 'Ana Maria Beto',
+        hosp: 'Hospital Santos Pereira',
+        endereco: 'Estr. São Francisco, 2008, Jardim Wanda, Taboão da Serra - SP, 06765-904',
+        valor: '74,99',
+        formacao: 'Faculdade de Medicina da Universidade de São Paulo',
+        dias: 'Segunda a Sábado',
+        hora: '9:00 - 18:00',
+        formato: 'Online / Presencial',
+      },
+    ],
+    2: [
+      {
+        id: 1,
+        foto: require('../imagens/medico/mdc5.png'),
+        rating: '⭐⭐⭐⭐⭐',
+        quantR: '(201)',
+        nome: 'Wedinilson Figueiredo Silva',
+        hosp: 'Psicologia Silva Santos',
+        endereco: 'R. Adelina Martins Piedade, 11 - Jardim Alvorada (Zona Oeste), São Paulo - SP, 05528-120',
+        valor: '190,90',
+        formacao: 'Faculdade de Medicina da Universidade de São Paulo',
+        dias: 'Segunda a Sexta',
+        hora: '10:00 - 18:00',
+        formato: 'Online',
+      },
+      {
+        id: 2,
+        foto: require('../imagens/medico/mdc1.png'),
+        rating: '⭐⭐⭐⭐⭐',
+        quantR: '(102)',
+        nome: 'Marcos Henrique Santos',
+        hosp: 'Psicologia Taboão da Serra',
+        endereco: 'Av. Vida Nova, 28 - 901 A, Jardim Maria Rosa, Taboão da Serra - SP, 06768-000',
+        valor: '180,00',
+        formacao: 'Faculdade de Medicina da Universidade de São Paulo',
+        dias: 'Domingo a Sexta',
+        hora: '9:00 - 18:00',
+        formato: 'Online / Presencial',
+      },
+      {
+        id: 3,
+        foto: require('../imagens/medico/mdc4.png'),
+        rating: '⭐⭐⭐⭐⭐',
+        quantR: '(309)',
+        nome: 'Ana Maria',
+        hosp: 'Hospital Equilíbrio',
+        endereco: 'R. Aldo Travaglia, 269 - Jardim Lar Sao Paulo, São Paulo - SP, 05639-120',
+        valor: '99,90',
+        formacao: 'Faculdade de Medicina da Universidade de São Paulo',
+        dias: 'Segunda a Sábado',
+        hora: '9:00 - 16:00',
+        formato: 'Online / Presencial',
+      },
+    ],
+  };
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity style={style.card} onPress={() => handlePress(item)}>
+      <View style={{flexDirection: 'column', alignItems: 'center', marginRight: 15}}>
+        <Image style={style.foto} source={item.foto}/>
+        <Text style={{fontSize: 14}}>{item.rating}</Text>
+        <Text style={style.rating}>{item.quantR}</Text>
+      </View>
+      <View style={{flexDirection: 'column'}}>
+        <Text style={style.nome}>{item.nome}</Text>
+        <Text style={style.text}>{item.hosp}</Text>
+        <View style={{flexDirection: 'row'}}>
+          <Icon name="map-marker" size={20} color="#E74C3C" style={{top: 5}}/>
+          <Text style={style.endereco}>{item.endereco}</Text>
         </View>
-    );
-};
+        <Text style={style.text}>R${item.valor}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+
+  const handlePress = (medico) => {
+    navigation.navigate('InfMedico', { medico });
+  };
+
+  return (
+    <View style={style.container}>
+      <View style={style.headerContainer}>
+        <TouchableOpacity
+          style={style.buttonInactive}
+          onPress={() => navigation.navigate('ConsultasFuturas')}
+          disabled={true}
+        >
+          <Text style={style.buttonText}>Agendamento</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={style.buttonActive}
+          onPress={() => navigation.navigate('ConsultasPassadas')}
+        >
+          <Text style={style.buttonText}>Suas Consultas</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text style={style.title}>Escolha o melhor dia para você!</Text>
+      <Text style={style.month}>Janeiro, 2024</Text>
+      <FlatList
+        data={data}
+        renderItem={renderItemDay}
+        keyExtractor={(item, index) => index.toString()}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={style.listContainer}
+      />
+
+      <Text style={style.title}>Com quem você quer falar?</Text>
+      <FlatList
+        style={{width: '90%', padding: 5}}
+        data={medPresentes[diaSelecionado]}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+        showsVerticalScrollIndicator={false}
+      />
+
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection:'column',
+    backgroundColor: 'white',
+    alignItems:'center',
+  },
+
+  headerContainer: {
+    flexDirection: 'row',
+    backgroundColor: '#5ce1e6',
+    borderRadius: 15,
+    paddingTop: 50,
+  },
+  buttonInactive: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingVertical: 15,
+    alignItems: 'center',
+    marginRight: 5,
+  },
+  buttonActive: {
+    flex: 1,
+    backgroundColor: '#5ce1e6',
+    paddingVertical: 15,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'black',
+    fontWeight: 'bold',
+  },
+
+  listContainer: {
+    paddingVertical: 20,
+    paddingHorizontal: 30,
+  },
+  item: {
+    width: 40,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#e0f7fa',
+    marginHorizontal: 5,
+    borderRadius: 20,
+    padding: 10,
+  },
+  itemText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+
+  title: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginTop: 30,
+    marginBottom: 7.5,
+  },
+  month: {
+    fontSize: 16,
+    marginVertical: 7.5,
+  },
+
+  card: {
+    padding: 10,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: '#c0c0c0',
+    marginVertical: 10,
+    width: '100%',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  foto: {
+    height: 70,
+    width: 70,
+    padding: 10,
+  },
+  rating: {
+    color: '#c0c0c0',
+    fontSize: 14,
+    marginTop: 5,
+  },
+  nome: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginVertical: 3,
+  },
+  text: {
+    fontSize: 16,
+    marginVertical: 3,
+  },
+  endereco: {
+    fontSize: 16,
+    marginVertical: 3,
+    width: '80%',
+    marginLeft: 3,
+  },
+});
--- RecoveryConnect/screens/consultasPassadas.js
+++ RecoveryConnect/screens/consultasPassadas.js
@@ -1,9 +1,183 @@
-import { View } from 'react-native';
+import {View, Text, TouchableOpacity, Image, StyleSheet, FlatList} from 'react-native';
 
-export default function ConsultasPassadas(){
-    return (
-        <View>
+export default function ConsultasPassadas({navigation}) {
+  const medFut = [
+    {
+      id: 1,
+      img: require('../imagens/medico/mdc1.png'),
+      nome: 'Marcos Henrique Santos',
+      hosp: 'Psicologia Taboão da Serra',
+      formato: 'Online',
+      data: '14 de janeiro',
+      hora: '15:00',
+    },
+    {
+      id: 2,
+      img: require('../imagens/medico/mdc1.png'),
+      nome: 'Marcos Henrique Santos',
+      hosp: 'Psicologia Taboão da Serra',
+      formato: 'Presencial',
+      data: '21 de janeiro',
+      hora: '10:00',
+    },
+    {
+      id: 3,
+      img: require('../imagens/medico/mdc3.png'),
+      nome: 'Ana Maria Beto',
+      hosp: 'Psicologia Pereira Santos',
+      formato: 'Online',
+      data: '30 de janeiro',
+      hora: '18:00',
+    },
+  ];
 
-        </View>
-    );
-};
+  const medPas = [
+    {
+      id: 1,
+      img: require('../imagens/medico/mdc3.png'),
+      nome: 'Ana Maria Beto',
+      hosp: 'Psicologia Pereira Santos',
+      formato: 'Online',
+      data: '28 de dezembro de 2023',
+      hora: '15:00',
+    },
+    {
+      id: 2,
+      img: require('../imagens/medico/mdc3.png'),
+      nome: 'Ana Maria Beto',
+      hosp: 'Psicologia Pereira Santos',
+      formato: 'Presencial',
+      data: '21 de dezembro de 2023',
+      hora: '10:00',
+    },
+    {
+      id: 3,
+      img: require('../imagens/medico/mdc2.png'),
+      nome: 'Laurêncio Gonzales Pinto',
+      hosp: 'Hospital Elma Mário',
+      formato: 'Presencial',
+      data: '01 de dezembro de 2023',
+      hora: '9:00',
+    },
+  ];
+
+  const renderItem = ({ item }) => (
+    <View style={style.card}>
+      <Image style={style.imagem} source={item.img}/>
+      <View style={{flexDirection: 'column'}}>
+        <Text style={style.nome}>{item.nome}</Text>
+        <Text style={style.hosp}>{item.hosp}</Text>
+        <Text style={style.text}>{item.formato}</Text>
+        <Text style={style.text}>{item.data}, às {item.hora}</Text>
+      </View>
+    </View>
+  );
+
+  return (
+    <View style={style.container}>
+      <View style={style.headerContainer}>
+        <TouchableOpacity
+          style={style.buttonActive}
+          onPress={() => navigation.navigate('ConsultasFuturas')}
+        >
+          <Text style={style.buttonText}>Agendamento</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={style.buttonInactive}
+          onPress={() => navigation.navigate('ConsultasPassadas')}
+          disabled={true}
+        >
+          <Text style={style.buttonText}>Suas Consultas</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text style={style.title}>Consultas futuras</Text>
+      <FlatList
+        style={{width: '90%', padding: 5}}
+        data={medFut}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+        showsVerticalScrollIndicator={false}
+      />
+
+      <Text style={style.title}>Consultas anteriores</Text>
+      <FlatList
+        style={{width: '90%', padding: 5}}
+        data={medPas}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection:'column',
+    backgroundColor: 'white',
+    alignItems:'center',
+  },
+
+  headerContainer: {
+    flexDirection: 'row',
+    backgroundColor: '#5ce1e6',
+    borderRadius: 15,
+    paddingTop: 50,
+  },
+  buttonInactive: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingVertical: 15,
+    alignItems: 'center',
+    marginRight: 5,
+  },
+  buttonActive: {
+    flex: 1,
+    backgroundColor: '#5ce1e6',
+    paddingVertical: 15,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'black',
+    fontWeight: 'bold',
+  },
+
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginTop: 30,
+    marginBottom: 10,
+  },
+
+  card: {
+    padding: 10,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: '#c0c0c0',
+    marginVertical: 10,
+    width: '100%',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  imagem: {
+    height: 70,
+    width: 70,
+    padding: 10,
+    marginRight: 15,
+  },
+  nome: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginVertical: 3,
+  },
+  hosp: {
+    fontSize: 18,
+    marginVertical: 3,
+  },
+  text: {
+    fontSize: 16,
+    marginVertical: 3,
+  },
+});
